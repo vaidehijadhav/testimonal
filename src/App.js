07608e6da322d1f56f915287a10d11dd578b13d2
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-04 22:15:50 +0530

    import reviews from data.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import reviews from "./data";
 
 const App = () => {
   return (
@@ -6,7 +7,7 @@ const App = () => {
       <div>
         <h1>Our Testimonials</h1>
         <div></div>
-        <Testimonial/>
+        <Testimonial reviews={reviews}/>
       </div>
     </div>
   );
