23f0ea9c0877caa39722824bae009d0a6bc8761e
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-04 21:56:17 +0530

    Add Testimonial component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonial/>
+      </div>
+    </div>
+  );
 };
 
 export default App;
