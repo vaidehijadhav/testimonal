130edfbc2ff8498cee7fa0c1c32054eb456f8b8e
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-07 23:42:55 +0530

    add icon for left right shift

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaQuoteLeft, FaQuoteRight, FaGem } from "react-icons/fa";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight} from "react-icons/fi";
 
 const Card = (props) =>{
     let review = props.review;
@@ -11,24 +12,33 @@ const Card = (props) =>{
                 <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute 
                 top-[-6px] z-[-20] left-[10px]"></div>
             </div>
-            <div>
-                <p>{review.name}</p>
+
+            <div className="text-center mt-7">
+                <p className="font-bold text-2xl capitalize">{review.name}</p>
             </div>
-            <div>
-                <p>{review.job}</p>
+
+            <div className="text-center mt-7">
+                <p className="text-violet-300 uppercase text-sm">{review.job}</p>
             </div>
-            <div>
+
+            <div className="text-violet-500 mx-auto mt-5">
                 <FaQuoteLeft/>
             </div>
-            <div>
+
+            <div className="text-center mt-4 text-slate-500">
                 {review.text}
             </div>
-            <div>
+
+            <div className="text-violet-500 mx-auto mt-5">
                 <FaQuoteRight/>
             </div>
             <div>
-                <button>+</button>
-                <button>-</button>
+                <button>
+                    <FiChevronLeft />
+                </button>
+                <button>
+                    <FiChevronRight />  
+                </button>
             </div>
             <div>
                 <button>Surprise Me</button>
