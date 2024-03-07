53ff804704b6042fe93d629fbe51a236ae294294
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-07 23:30:14 +0530

    Add tailwind CSS to heading and image

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,12 @@ import { FaQuoteLeft, FaQuoteRight, FaGem } from "react-icons/fa";
 const Card = (props) =>{
     let review = props.review;
     return(
-        <div>
-            <div>
-                <img src={review.image}/>
+        <div className="flex flex-col md:relative">
+            <div className="absolute top-[-7rem] z-[10] mx-auto">
+                <img className="aspect-square rounded-full w-[140px] h-[140px] z-[25]" 
+                src={review.image}/>
+                <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute 
+                top-[-6px] z-[-20] left-[10px]"></div>
             </div>
             <div>
                 <p>{review.name}</p>
