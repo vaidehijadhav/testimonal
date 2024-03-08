75bf97273df47cdcb0c555da6a98b613186d15a8
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-08 19:19:38 +0530

    done minor changes

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -31,10 +31,11 @@ const Testimonial = (props) => {
     }
 
     return(
-        <div>
+        <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center 
+        mt-10 p-10 rounded-md transition-all duration-700 hover:shadow-xl">
             <Card review = {reviews[index]}></Card>
             
-                <div className="flex text-3xl mt-5 gap-3 text-violet-300 font-bold mx-auto">
+                <div className="flex text-3xl mt-10 gap-3 text-violet-300 font-bold mx-auto">
                     <button onClick={leftShiftHandler} className="cursor-pointer hover:text-violet-500">
                         <FiChevronLeft />
                     </button>
@@ -43,7 +44,7 @@ const Testimonial = (props) => {
                     </button>
                 </div>
 
-                <div>
+                <div className="mt-5">
                     <button onClick={surpriseHandler} className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 
                     cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
                         Surprise Me
