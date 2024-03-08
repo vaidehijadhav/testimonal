d43bc50dd289346ec9fb6e95f673c1f10b61c745
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-08 18:45:49 +0530

    create different functions

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
+import { FiChevronLeft, FiChevronRight} from "react-icons/fi";
+
 
 const Testimonial = (props) => {
     let reviews = props.reviews;
+    const[index, setIndex] = useState(0);
+
+    function leftShiftHandler(){
+        if(index-1 < 0){
+            setIndex(reviews.length - 1);
+        }
+        else{
+            setIndex(index -1);
+        }
+    }
+
+    function rightShiftHandler(){
+        if(index + 1 >=reviews.length){
+            setIndex(0);
+        }
+        else{
+            setIndex(index + 1);
+        }
+    }
+
+    function surpriseHandler(){
+        let randomIndex = Math.floor(Math.random()*reviews.length);
+        setIndex(randomIndex);
+    }
+
     return(
-        <Card review={reviews[0]}></Card>
+        <div>
+            <Card review={reviews[0]}></Card>
+            
+                <div className="flex text-3xl mt-5 gap-3 text-violet-300 font-bold mx-auto">
+                    <button onClick={leftShiftHandler} className="cursor-pointer hover:text-violet-500">
+                        <FiChevronLeft />
+                    </button>
+                    <button onClick={rightShiftHandler} className="cursor-pointer hover:text-violet-500">
+                        <FiChevronRight />  
+                    </button>
+                </div>
+
+                <div>
+                    <button onClick={surpriseHandler} className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 
+                    cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
+                        Surprise Me
+                    </button>
+                </div>
+            </div>    
     );
 };
 
